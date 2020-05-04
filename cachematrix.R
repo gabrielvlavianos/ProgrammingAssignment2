c68f8a141d1ebdf11642af080e962d1bcd09059b
gabrielvlavianos <64794797+gabrielvlavianos@users.noreply.github.com>
2020-05-04 14:18:17 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The function was based on the "mean" example. But instead of calculating the mean
+## it calculates de inverse of a matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+  makeCacheMatrix <- function(x = matrix()) {
+    z <- NULL
+    set <- function(y){
+      x <<- y
+      z <<- NULL
+    }
+    get <- function()x
+    set_inverse <- function(inverse) z <<- inverse
+    get_inverse <- function() z 
+    list(set = set, get = get, 
+         set_inverse = set_inverse, 
+         get_inverse = get_inverse)
+  }
 
-}
 
-
-## Write a short comment describing this function
+## This functions checks if the inverse of the matrix was already calculated. If so, it get's the outcome and print it. 
+  ##if not, it calculates it and prints the result.
+ 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  z <- x$get_inverse()
+  if(!is.null(z)){
+    message("getting cache data")
+    return(z)
+  }
+  m <- x$get()
+  z <- solve(m,...)
+  x$set_inverse(z)
+  z
 }
